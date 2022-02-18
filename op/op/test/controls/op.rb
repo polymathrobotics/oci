ef0bfdbe173052c217b481d18a7fb55c07f736e0
Mischa Taylor <taylor@linux.com>
2022-02-18 10:19:42 -0500

    Add parallel command to op image

--- op/op/test/controls/op.rb
+++ op/op/test/controls/op.rb
@@ -1,7 +1,23 @@
-describe command('op') do
-  it { should exist }
+%w(
+  jq
+  op
+  parallel
+).each do |cmd| 
+  describe command(cmd) do
+    it { should exist }
+  end
 end
 
 describe command('op --version') do
   its('exit_status') { should eq 0 }
+end
+
+describe command('jq --version') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match (/jq-/ }
+end
+
+describe command('parallel --version') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { shoud match (/GNU parallel/ }
 end
