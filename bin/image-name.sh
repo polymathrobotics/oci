ce207d5c4b654cd8a056e6e86281371075f291c3
Mischa Taylor <taylor@linux.com>
2022-03-30 19:18:51 -0400

    Pin to a specific image version

--- bin/image-name.sh
+++ bin/image-name.sh
@@ -3,13 +3,13 @@
 CONTAINERFILE_DIR=$(pwd)
 IMAGE_NAME=$(basename $(pwd))
 
-DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-amd64:1.24.1
+DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-amd64
 if [ "$(uname -s)" = Darwin ] && [ "$(uname -m)" = arm64 ]; then
-  DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-arm64:1.24.1
+  DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-arm64
 fi
-# No need to pull on every build because we're referring to a specific tag
 
 if [[ -f "${CONTAINERFILE_DIR}/Polly.toml" ]]; then
+  docker image pull ${DASEL_CONTAINER_IMAGE} > /dev/null 2>&1
   _name=$(docker container run --rm \
     --mount type=bind,source="$(pwd)",target=/share,readonly \
     ${DASEL_CONTAINER_IMAGE} \
