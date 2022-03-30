7643785297541ec86cbeaedf012227c7cd9ae94f
Mischa Taylor <taylor@linux.com>
2022-03-30 14:31:48 -0400

    Use toml files instead of IMAGE_NAME and TAGS

--- bin/image-name.sh
+++ bin/image-name.sh
@@ -2,8 +2,20 @@
 
 CONTAINERFILE_DIR=$(pwd)
 IMAGE_NAME=$(basename $(pwd))
+DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-amd64
+if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple" ]]; then
+  DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-arm64
+fi
 
-if [[ -f "${CONTAINERFILE_DIR}/IMAGE_NAME" ]]; then
-  IMAGE_NAME=$(head -n 1 "${CONTAINERFILE_DIR}/IMAGE_NAME")
+if [[ -f "${CONTAINERFILE_DIR}/Polly.toml" ]]; then
+  name=$(docker container run -t --rm \
+    --mount type=bind,source="$(pwd)",target=/share,readonly \
+    polymathrobotics/dasel-arm64 \
+      -f Polly.toml --null "container_image.name")
+  name=$(echo ${name} | tr -d '\r')
+
+  if [ "${name}" != "null" ]; then
+    IMAGE_NAME=${name} 
+  fi
 fi
 echo ${IMAGE_NAME}
--- bin/list-tags.sh
+++ bin/list-tags.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-set -x
-
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BIN_DIR="${SCRIPT_PATH}"
 CONTAINERFILE_DIR=$(pwd)
 IMAGE_NAME="$(${BIN_DIR}/image-name.sh)"
 CONTAINER_REGISTRY="docker.io/polymathrobotics"
 DEFAULT_TAG="${CONTAINER_REGISTRY}/${IMAGE_NAME}"
-
-tags="${DEFAULT_TAG}"
-if [[ -f "${CONTAINERFILE_DIR}/TAGS" ]]; then
-  tags=$(sed 's/#.*//' "${CONTAINERFILE_DIR}/TAGS" | sed '/^[[:space:]]*$/d' | sed "s|^|${DEFAULT_TAG}:| ")
+DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-amd64
+if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple" ]]; then
+  DASEL_CONTAINER_IMAGE=polymathrobotics/dasel-arm64
 fi
-for tag in ${tags}; do
-  echo "${tag}"
-done
+
+docker container run -t --rm \
+  --mount type=bind,source="$(pwd)",target=/share,readonly \
+  polymathrobotics/dasel-arm64 \
+    -f Polly.toml -w json | jq -r '.container_image.tags | .[]'
