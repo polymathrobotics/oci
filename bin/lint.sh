ad2e3e394405a0121321f3ba6ffb9e71e50178d5
Mischa Taylor <taylor@linux.com>
2022-04-16 21:09:17 -0400

    Add hadolint-arm64 image

--- bin/lint.sh
+++ bin/lint.sh
@@ -7,6 +7,9 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BIN_DIR="${SCRIPT_PATH}"
 BASE_DIR=$(pwd)
 HADOLINT_CONTAINER_IMAGE=polymathrobotics/hadolint-amd64
+if [ "$(uname -s)" = Darwin ] && [ "$(uname -m)" = arm64 ]; then
+  HADOLINT_CONTAINER_IMAGE=polymathrobotics/hadolint-arm64
+fi
 
 lints_to_ignore=$(sed "s/#.*//" "${BIN_DIR}/hadolint-ignore" | sed '/^[[:space:]]*$/d' | sed 's/^/--ignore / ' | tr '\n' ' ')
 docker container run --rm -i ${HADOLINT_CONTAINER_IMAGE} hadolint ${lints_to_ignore} - < "Containerfile"
