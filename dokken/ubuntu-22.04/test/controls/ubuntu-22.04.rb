4eb67f6f20876c29358b4f32adda53731d974e1d
Mischa Taylor <taylor@linux.com>
2022-05-05 09:50:49 -0400

    Remove test for timesyncd

--- dokken/ubuntu-22.04/test/controls/ubuntu-22.04.rb
+++ dokken/ubuntu-22.04/test/controls/ubuntu-22.04.rb
@@ -1,9 +1,3 @@
 describe command('lsb_release --release') do
   its('stdout') { should match(/22\.04/) }
 end
-
-# systemd-timesyncd is no longer automatically installed when
-# the systemd package is installed
-describe package('systemd-timesyncd') do
-  it { should be_installed }
-end
