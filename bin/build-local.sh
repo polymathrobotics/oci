0a5ffc13c44c17604f9d3c04666e3868f37d0e30
Mischa Taylor <taylor@linux.com>
2022-10-28 15:57:43 -0400

    Fix issue with old variable name in script

--- bin/build-local.sh
+++ bin/build-local.sh
@@ -6,7 +6,7 @@ set -o pipefail
 BIN_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 DEFAULT_TAG="$("${BIN_DIR}/list-tags.sh" | head -n 1)"
 
-echo "==> Building ${FULL_IMAGE_NAME}"
+echo "==> Building ${DEFAULT_TAG}"
 docker buildx build \
   --load \
   --file Containerfile \
