d1cf19b7320083a054cddb23f2593b6fe764caf7
Mischa Taylor <taylor@linux.com>
2023-10-18 18:24:37 -0400

    Would help if I wrote the code in ruby

--- jfrog/jfrog-cli/test/controls/jf.rb
+++ jfrog/jfrog-cli/test/controls/jf.rb
@@ -12,9 +12,9 @@ describe user('jfrog') do
   its('uid') { should eq 1000 }
   its('gid') { should eq 1000 }
   its('home') { should eq '/bin/bash' }
-}
+end
 
 describe directory('/home/jfrog') do
   its('owner') { should eq 1000 }
   its('group') { should eq 1000 }
-}
+end
