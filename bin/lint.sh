7ad5ef7292cb0fd0958cc0f162e20b6160b86232
Mischa Taylor <taylor@linux.com>
2023-11-24 09:44:15 -0500

    Change lint script to use bake

--- bin/lint.sh
+++ bin/lint.sh
@@ -3,17 +3,18 @@
 set -eu
 set -o pipefail
 
-HADOLINT_CONTAINER_IMAGE=polymathrobotics/hadolint:2.10.0
+HADOLINT_CONTAINER_IMAGE=docker.io/polymathrobotics/hadolint:2.10.0
 
 BIN_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-"${BIN_DIR}/check-image.sh" "${HADOLINT_CONTAINER_IMAGE}"
+# "${BIN_DIR}/check-image.sh" "${HADOLINT_CONTAINER_IMAGE}"
 
 # shellcheck disable=SC2086
-if [[ -f "${BIN_DIR}/hadolint-ignore" ]]; then
-  lints_to_ignore=$(sed "s/#.*//" "${BIN_DIR}/hadolint-ignore" | sed '/^[[:space:]]*$/d' | sed 's/^/--ignore / ' | tr '\n' ' ')
-  docker container run --rm -i "${HADOLINT_CONTAINER_IMAGE}" hadolint ${lints_to_ignore} - < "Containerfile"
-  exit 0
-fi
+# if [[ -f "${BIN_DIR}/hadolint-ignore" ]]; then
+#   lints_to_ignore=$(sed "s/#.*//" "${BIN_DIR}/hadolint-ignore" | sed '/^[[:space:]]*$/d' | sed 's/^/--ignore / ' | tr '\n' ' ')
+#   docker container run --rm -i "${HADOLINT_CONTAINER_IMAGE}" hadolint ${lints_to_ignore} - < "Containerfile"
+#   exit 0
+# fi
 
-docker container run --rm -i "${HADOLINT_CONTAINER_IMAGE}" hadolint - < "Containerfile"
+docker container run --rm -i \
+  "${HADOLINT_CONTAINER_IMAGE}" hadolint - < "Containerfile"
