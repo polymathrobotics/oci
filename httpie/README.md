c278bd540e2ab4beb0d597f04325a5e8766b3d8b
Mischa Taylor <taylor@linux.com>
2023-11-27 08:34:37 -0500

    Remove httpie image

--- httpie/README.md
+++
@@ -1,9 +0,0 @@
-# httpie
-
-## Examples
-Hello World:
-
-```
-docker run --rm polymathrobotics/httpie \
-  httpie.io/hello
-```
--- httpie/test/controls/httpie.rb
+++
@@ -1,4 +0,0 @@
-describe command('http --help') do
-  its('exit_status') { should eq 0 }
-  its('stdout') { should match(/HTTPie/) }
-end
