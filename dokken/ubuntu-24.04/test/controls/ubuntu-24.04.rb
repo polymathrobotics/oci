dc678d10fc8e076032cf88406480bf9ad757bcaf
Mischa Taylor <taylor@linux.com>
2024-04-26 15:34:45 -0400

    Add ubuntu 24.04 dokken image

---
+++ dokken/ubuntu-24.04/test/controls/ubuntu-24.04.rb
@@ -0,0 +1,9 @@
+describe command('lsb_release --release') do
+  its('stdout') { should match(/24\.04/) }
+end
+
+# systemd-timesyncd is no longer automatically installed when
+# the systemd package is installed
+describe package('systemd-timesyncd') do
+  it { should be_installed }
+end
