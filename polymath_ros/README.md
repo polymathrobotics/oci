54b46a9d7aa5a026fe094d50280640b836675870
David Tarazi <dtarazi@olin.edu>
2024-09-12 14:05:13 -0700

    added polymath deps to polymath_ros containers

---
+++ polymath_ros/README.md
@@ -0,0 +1,3 @@
+# Polymath_ROS - Polymath specific packages built on top of ROS
+
+polymath_ros images contain wants and needs for Polymath Robotics' stack to build on top of.
---
+++ polymath_ros/jazzy/ubuntu/noble/test/controls/polymath_ros_jazzy.rb
@@ -0,0 +1,12 @@
+image_value = input('test_container_image')
+puts "MISCHA: #{image_value}"
+
+
+describe command("su --login --command \"source /opt/ros/$ROS_DISTRO/setup.bash && ros2 pkg list | grep rmw_cyclonedds_cpp\"") do
+  its('exit_status') { should cmp 0 }
+end
+
+describe command("pip show setuptools") do
+  its('exit_status') { should cmp 0 }
+  its('stdout') { should match(/Version: 58.2.0/) }
+end
