0abb08b34473b390b10a2dd42a77d745fc769896
Mischa Taylor <taylor@linux.com>
2024-09-13 10:44:21 -0400

    There is no python3-rosinstall for jazzy

--- polymath-ros/jazzy/ubuntu/noble/test/controls/polymath-ros.rb
+++ polymath-ros/jazzy/ubuntu/noble/test/controls/polymath-ros.rb
@@ -2,22 +2,18 @@ image_value = input('test_container_image')
 
 describe os do
   its('name') { should eq 'ubuntu' }
-  its('release') { should eq '22.04' }
+  its('release') { should eq '24.04' }
 end
 
 describe os_env('ROS_DISTRO') do
-  its('content') { should include 'humble' }
+  its('content') { should include 'jazzy' }
 end
 
 describe command("su --login --command \"source /opt/ros/$ROS_DISTRO/setup.bash && ros2 pkg list | grep rmw_cyclonedds_cpp\"") do
   its('exit_status') { should cmp 0 }
 end
 
-describe command("pip show setuptools") do
-  its('exit_status') { should cmp 0 }
-  its('stdout') { should match(/Version: 65/) }
-end
-
-describe command("pip show wheel") do
-  its('exit_status') { should cmp 0 }
+describe command('pip list') do
+  its('stdout') { should match /setuptools/ }
+  its('stdout') { should match /wheel/ }
 end
