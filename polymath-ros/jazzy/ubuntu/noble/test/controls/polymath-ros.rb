debe4a65f3b446f41f4d532a16e9f870ae68c408
David Tarazi <dtarazi@olin.edu>
2025-02-13 14:55:44 -0800

    Updating the container environment to have setuptools and pyenv (#5)
    * added polymath deps to polymath_ros containers
    
    * update polymath ros

--- polymath-ros/jazzy/ubuntu/noble/test/controls/polymath-ros.rb
+++ polymath-ros/jazzy/ubuntu/noble/test/controls/polymath-ros.rb
@@ -9,11 +9,15 @@ describe os_env('ROS_DISTRO') do
   its('content') { should include 'jazzy' }
 end
 
-describe command("su --login --command \"source /opt/ros/$ROS_DISTRO/setup.bash && ros2 pkg list | grep rmw_cyclonedds_cpp\"") do
+describe command("which python") do
   its('exit_status') { should cmp 0 }
+  its('stdout') { should match /\/opt\/venv\/bin\/python/ }
 end
 
 describe command('pip list') do
   its('stdout') { should match /setuptools/ }
-  its('stdout') { should match /wheel/ }
+end
+
+describe command("su --login --command \"source /opt/ros/$ROS_DISTRO/setup.bash && ros2 pkg list | grep rmw_cyclonedds_cpp\"") do
+  its('exit_status') { should cmp 0 }
 end
