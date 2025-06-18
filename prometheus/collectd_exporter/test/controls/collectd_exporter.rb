615c2b46bc548184c86454f06597831a37c346d5
Mischa Taylor <taylor@linux.com>
2025-06-18 16:38:49 -0400

    Remove collectd_exporter image

--- prometheus/collectd_exporter/test/controls/collectd_exporter.rb
+++
@@ -1,8 +0,0 @@
-describe command('collectd_exporter') do
-  it { should exist }
-end
-
-describe command('collectd_exporter -h') do
-  its('exit_status') { should eq 0 }
-  its('stderr') { should match(/usage: collectd_exporter/) }
-end
